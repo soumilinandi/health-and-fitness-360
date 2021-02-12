71b0e838a9d0f004f8f5c7e51fe8b8d568b82457
Soumili Nandi <soumilinandi99@gmail.com>
2021-02-12 12:36:02 +0530

    final app commit

---
+++ js/reminder.js
@@ -0,0 +1,8 @@
+$( function() {
+    $( "#datepicker" ).datepicker();
+} );
+// Used to send reminder data to database
+function displayData() {
+  document.getElementById('displayresult').innerHTML = "Reminder added!";
+  //get elements by id and add to databse
+}
